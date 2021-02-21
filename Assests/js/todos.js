e4bdea8a3b4776eb4eeee6ae265ea06c69633257
Csaba1995 <61435013+Csaba1995@users.noreply.github.com>
2021-02-21 19:09:14 +0100

    Add files via upload

---
+++ Assests/js/todos.js
@@ -0,0 +1,28 @@
+$("ul").on("click", "li", function(){
+	$(this).toggleClass("kurwo");
+	});
+
+
+//CLick on X to delete to do
+$("ul").on("click", "span", function(event){
+	$(this).parent().fadeOut( 500,function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
+
+
+$("input[type='text'").keypress(function(event){
+	if (event.which === 13){
+		//grabing new todo text from input
+		var todoText = $(this).val();
+		$(this).val ("");
+		//creat a new li and add to ul
+		$("ul").append("<li><span ><i class='fa fa-trash'></i></span> "+ todoText + "</li>")
+	}
+
+})
+
+$(".fa-plus-square").click(function(){
+	$("input[type='text'").fadeToggle();
+})
